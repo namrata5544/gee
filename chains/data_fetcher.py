a7b256ff9abb2be68b7c60767e379c9f700768cd
tanishbajaj101 <tanishbajaj101@gmail.com>
2025-07-13 19:48:01 +0530

    removed unnecessary file

--- chains/data_fetcher.py
+++
@@ -1,9 +0,0 @@
-from chains.code_generator import generate_gee_code
-from utils.sandbox_runner import run_in_sandbox
-
-def fetch_data_from_gee(region, start, end, dataset):
-    code = generate_gee_code(region, start, end, dataset)
-    result = run_in_sandbox(code)
-    with open("output/gee_output.json", "w") as f:
-        f.write(result)
-    return result
